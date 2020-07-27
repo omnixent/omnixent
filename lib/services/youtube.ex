d6e7b526fbfe62c9dc0ebebe22c656f5efb423f1
Michele Riva <master@micheleriva.it>
2020-07-27 16:25:40 +0200

    feat(youtube): added youtube support

--- lib/services/youtube.ex
+++ lib/services/youtube.ex
@@ -1,12 +1,18 @@
 defmodule Omnixent.Services.Youtube do
 
   @youtube_endpoint    "https://clients1.google.com/complete/search"
-  @youtube_queryparams "?client=youtube&gs_ri=youtube"
+  @youtube_queryparams "&client=youtube&gs_ri=youtube"
+
+  def search(term, country \\ "en", language \\ "en") do
+    Omnixent.Languages.read_languages_file(language)
+      |> Enum.map(& String.replace(&1, "@", term))
+      |> Enum.map(& call_youtube(&1, country, language))
+  end
 
   def call_youtube(term, country, language) do
     case format_youtube_uri(term, country, language) |> HTTPoison.get do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, term, body}
+        {:ok, term, extract_youtube_body(body)}
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:error, 404}
@@ -19,8 +25,18 @@ defmodule Omnixent.Services.Youtube do
       end
   end
 
+  def extract_youtube_body(result) do
+    result
+      |> String.replace(~r/^window\.google\.ac\.h\(/, "")
+      |> String.replace(~r/\)$/, "")
+      |> Omnixent.Utils.safe_json_decode
+      |> Enum.at(1)
+      |> Enum.map(& hd(&1))
+  end
+
   def format_youtube_uri(term, country \\ "en", language \\ "en") do
     @youtube_endpoint
+      <> "?q="
       <> URI.encode(term)
       <> @youtube_queryparams
       <> "hl="
--- mix.exs
+++ mix.exs
@@ -1,20 +1,36 @@
 defmodule Omnixent.MixProject do
   use Mix.Project
 
+  @app     :omnixent
+  @name    "Omnixent"
+  @version "0.0.1"
+  @github  "https://github.com/omnixent/#{@app}"
+
   def project do
     [
-      app: :omnixent,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      app:             @app,
+      version:         @version,
+      elixir:          "~> 1.10",
+      build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps:            deps(),
+
+      package:         package(),
+      description:     description(),
+      source_url:      @github,
+      homepage_url:    @github,
+      docs: [
+        main:          @name,
+        canonical:     "https://hexdocs.pm/#{@app}",
+        extras:        ["README.md"]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mnesia]
     ]
   end
 
@@ -24,7 +40,21 @@ defmodule Omnixent.MixProject do
       {:httpoison, "~> 1.6"},
       {:poison,    "~> 3.1"},
       {:memento,   "~> 0.3.1"},
-      {:timex,     "~> 3.5"}
+      {:timex,     "~> 3.5"},
+      {:ex_doc,    ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp package() do
+    [
+      author:      "Michele Riva <ciao@micheleriva.it>",
+      maintainers: ["Michele Riva"],
+      licenses:    ["GPLv3"],
+      links:       %{"GitHub" => "https://github.com/omnixent/omnixent"}
+    ]
+  end
+
+  defp description() do
+    "A simple API which lets you to understand what people searches on the internet."
   end
 end
