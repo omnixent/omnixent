d688a0ab747c1a372d023d7ab70498e760910ed8
thejoin95 <miki.lombi@gmail.com>
2021-03-14 22:32:07 +0100

    feat(services): change to mkt parameters for a better locale accuracy

--- src/lib/services/bing.ts
+++ src/lib/services/bing.ts
@@ -4,8 +4,8 @@ import { normalizeServiceArgs } from "../utils/index.ts";
 const endpoint = "https://api.bing.com/osjson.aspx";
 
 export function formatURI(options: ServiceArgs): string {
-  const { t, hl } = normalizeServiceArgs(options);
-  return `${endpoint}?query=${t}&cc=${hl}`;
+  const { t, c, l } = normalizeServiceArgs(options);
+  return `${endpoint}?query=${t}&mkt=${l}-${c}`;
 }
 
 export function extractBody(response: string) {
