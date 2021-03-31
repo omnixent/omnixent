2b14e65fbaa40a0b2395bcd95fdf9002ca7b5bce
Michele Riva <master@micheleriva.it>
2021-03-31 22:01:19 +0200

    feat(server): added response-time middleware

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import responseTime from 'response-time';
 import * as dotenv from 'dotenv';
 import apiV1PublicRoutes from './routes/v1/public';
 import apiV1PrivateRoutes from './routes/v1/private';
@@ -14,11 +15,13 @@ export default function init() {
 
   app.use(cors());
   app.use(compression());
+  app.use(responseTime());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use('/v1', apiV1PublicRoutes);
   app.use('/v2', apiV1PrivateRoutes);
+  app.use('/', (_, res) => res.json({ success: true }));
 
   app.listen(port, () => {
     console.log(`Omnixent is running at http://localhost:${port}`);
