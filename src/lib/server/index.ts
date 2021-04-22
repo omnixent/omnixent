6a35f5b0c722b1876049dd98bd9bac5d984e789d
Michele Riva <master@micheleriva.it>
2021-04-22 12:53:41 +0200

    fix(cors): made cors headers explicit

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -14,14 +14,17 @@ export default function init() {
   const app = express();
   const port = process.env.PORT || 3000;
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: '*',
-      exposedHeaders: '*',
-    }),
-  );
-
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
+    );
+    next();
+  });
   app.use(compression());
   app.use(responseTime());
   app.use(bodyParser.json());
